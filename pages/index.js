82b0a086545ba58e45be0dd59b3845a820c4c39b
Gilmar Junior <grdev.facul@gmail.com>
2026-01-22 11:01:44 +0000

    sorrizinho

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>❤️❤️Joelma, eu amo você ❤️❤️... Se você me ama, dá uma risadinha!</h1>
+    return <h1>❤️❤️Joelma, eu amo você ❤️❤️... Se você me ama, dá uma risadinha! 😊</h1>
 }
 
 export default Home;
